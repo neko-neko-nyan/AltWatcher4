ff06eb4b058ef307095fb1a23c8ddfe2bce7ed33
Pasha13666 <Pasha13666@users.noreply.github.com>
2019-12-20 14:46:12 +0300

    Update watchonlinemenu.user.js

--- watchonlinemenu.user.js
+++ watchonlinemenu.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         WatchOnlineMenu
 // @namespace    https://openuserjs.org/users/Pasha13666
-// @version      4.1.7
+// @version      4.1.8
 // @description  [shikimori.org] Добавляет ссылки на сайты просмотра аниме
 // @author       Pasha13666
 // @match        http://shikimori.one/*
@@ -23,7 +23,7 @@
 // @copyright    2019, Pasha13666 (https://openuserjs.org/users/Pasha13666)
 // ==/UserScript==
 
-/* div.watch-online-placeholer (from shiki) - $ph
+/* div - $ph
  *   div - $wo
  *     a.aw4-dark-button.aw4-link - $link
  *     a.aw4-dark-button.aw4-hider - $hider
@@ -39,13 +39,14 @@
 function WatchOnlineMenu(services) {
     this.services = services;
 
-    this.$ph = document.getElementsByClassName('watch-online-placeholer')[0];
-    this.$wo = document.createElement('div')
+    this.$ph = document.getElementsByClassName('watch-online')[0];
+    this.$rb = document.getElementsByClassName("c-info-right")[0];
+    this.$wo = document.createElement('div');
     this.$link = document.createElement('a');
     this.$hider = document.createElement('a');
-    this.$clearfix = document.createElement('div')
-    this.$bar = document.createElement('div')
-    this.$langs = document.createElement('div')
+    this.$clearfix = document.createElement('div');
+    this.$bar = document.createElement('div');
+    this.$langs = document.createElement('div');
     this.$ru = document.createElement('a');
     this.$en = document.createElement('a');
 
@@ -70,6 +71,13 @@ function WatchOnlineMenu(services) {
     else this.$en.classList.add('aw4-selected');
 
     this.isHentai = document.querySelector('a.b-tag[href*="genre/12"]') != null;
+
+    if (this.$ph !== undefined){
+        this.$rb.removeChild(this.$ph);
+    }
+
+    this.$ph = document.createElement('div');
+    this.$rb.appendChild(this.$ph);
 
     for (var i = 0; i < this.services.length; i++) {
         var v = this.services[i];
@@ -176,7 +184,7 @@ function checkUpdates(){
                     else GM_log("[WatchOnlineMenu] Not starting WatchOnlineMenu due to network error.");
                 } else {
                     GM_setValue("servicesCache", {services: obj.response, date: +new Date() + 1000 * 60 * 60 * 24});
-                    res(obj.response)
+                    res(obj.response);
                 }
             }
         });
@@ -184,7 +192,7 @@ function checkUpdates(){
 }
 
 document.head.appendChild(GM_addStyle(
-    ".aw4-dark-button { background-color: #456; border: 1px solid #3e4d5d; color: #fff; display: block; text-align: center; cursor: pointer; padding: 8px 0; font-size: 15px; font-weight: 600; }" +
+    ".aw4-dark-button { background-color: #456; border: 1px solid #3e4d5d; color: #fff; display: block; text-align: center; cursor: pointer; padding: 4px 0; font-size: 15px; font-weight: 600; }" +
     ".aw4-link  {float: left; width: calc(100% - 32px); margin-bottom: 15px; }\n" +
     ".aw4-hider {width: 32px; float: right; }\n" +
     ".aw4-lang { width: 50%; float: left; }\n" +
